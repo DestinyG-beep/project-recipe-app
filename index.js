478b2c26c57354aeac7bf042e50f7f88430a22a2
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-10-18 04:47:38 +0300

    adds form contents

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const loginForm = document.getElementById('login-form');
+    const recipeForm = document.getElementById('recipe-form');
+    const searchForm = document.getElementById('search-form');
